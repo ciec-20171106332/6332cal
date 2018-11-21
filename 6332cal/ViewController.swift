51f7a0a18d81648f2875e49194a8bf5a1b27afa9
ciecS20171106332 <2690603728@qq.com>
2018-11-21 08:38:30 +0800

    Initial Commit

---
+++ 6332cal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  6332cal
+//
+//  Created by s20171106332 on 2018/11/21.
+//  Copyright © 2018 s20171106332. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
